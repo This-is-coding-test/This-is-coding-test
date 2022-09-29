d7059919c2226abe24bff60a2b56a2eb1c586bfe
yoon <dudwls143@gmail.com>
2022-09-29 20:18:39 +0900

    Ch6 두 배열의 원소 교체 완료

---
+++ Sorting/src/두_배열의_원소_교체.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 두_배열의_원소_교체 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Integer[] A = new Integer[N];
+        Integer[] B = new Integer[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        Arrays.sort(B, (o1, o2) -> o2 - o1);
+        for (int i = 0; i < K; i++) {
+            if (A[i] < B[i]) {
+                A[i] = B[i];
+            }
+        }
+        int sum = 0;
+
+        for (Integer x : A) {
+            sum += x;
+        }
+        System.out.println(sum);
+
+    }
+}
